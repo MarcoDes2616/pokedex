ec92ed5c99817a01647a4ac7a585eec90b696f91
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-01-21 07:22:17 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 import { getUsername } from '../store/slices/username.slice'
+import ballup from "../assets/img/pb_up.png"
+import balldown from "../assets/img/pb_down.png"
+import screen from "../assets/img/screenpd.png"
 
 const Home = () => {
     const navigate = useNavigate();
@@ -19,11 +22,18 @@ const Home = () => {
     }
 
     return (
-        <div>
-            <h1>home</h1>
-            <h2>{username}</h2>
-            <input type="text" value={inputName} onChange={e => setImputName(e.target.value)} />
-            <button onClick={getPokedex}>Ingresar</button>
+        <div className='home'>
+            <div className='input__container'>
+                <img src={ballup} alt="" />
+                <div className='screen'>
+                    <img src={screen} alt="" />
+                    <div className='input'>
+                        <input placeholder='name' type="text" value={inputName} onChange={e => setImputName(e.target.value)} />
+                        <button onClick={getPokedex}><i className='bx bxl-go-lang bx-tada bx-lg' ></i></button>
+                    </div>
+                </div>
+                <img src={balldown} alt="" />
+            </div>
         </div>
     );
 };
